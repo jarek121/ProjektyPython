1a7fba22e88c4047d6180121f0419c4b15eb0a07
Jarosław Hyżyk <jaroslawhyzyk@gmail.com>
2026-04-26 21:23:14 +0200

    dodanie listy zakupów

---
+++ zakupy Signore Anguria.py
@@ -0,0 +1,9 @@
+zakupy = ["Pomidory", "Ser", "Bazylia"]
+# print(f"Pan Arbuz sprawdza listę: {zakupy}")
+zakupy.append("oliwa")
+zakupy.remove("Pomidory")
+zakupy.reverse()
+print(f"Pan Arbuz sprawdza listę: {zakupy}")
+# for wypis in zakupy:
+#     print(f"Kupić: {wypis}")
+# print("LISTA JEST KOMPLETNA")
