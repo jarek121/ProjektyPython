03a311a7b30ec5699c94d6489370f58c21c5266b
Jarosław Hyżyk <jaroslawhyzyk@gmail.com>
2026-04-26 21:42:44 +0200

    dodanie zabezpieczenie

--- zakupy Signore Anguria.py
+++ zakupy Signore Anguria.py
@@ -2,7 +2,12 @@ zakupy = ["Pomidory", "Ser", "Bazylia"]
 # print(f"Pan Arbuz sprawdza listę: {zakupy}")
 zakupy.append("oliwa")
 zakupy.remove("Pomidory")
-zakupy.reverse()
+# zakupy.reverse()
+for produkt in zakupy:
+    if produkt == "Bazylia":
+        print("Zygfryd! odłóż tę bazylię!")
+    else:
+        print(f"{produkt} jest bezpieczny w koszyku.")
 print(f"Pan Arbuz sprawdza listę: {zakupy}")
 # for wypis in zakupy:
 #     print(f"Kupić: {wypis}")
