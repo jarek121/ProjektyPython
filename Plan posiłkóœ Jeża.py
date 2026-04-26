e0fe42360ae9d3559d31be26cfde4c5df8f730af
Jarosław Hyżyk <jaroslawhyzyk@gmail.com>
2026-04-26 16:19:30 +0200

    add pear

--- Plan posiłkóœ Jeża.py
+++ Plan posiłkóœ Jeża.py
@@ -1,2 +1,7 @@
-for days in range(1, 8): 
-    print(f"Dzień {days}: Pan Jeż zajada jabłuszko")
+for days in range(1, 8):
+    if days == 7:
+        print(f"Dzień {days}: Pan Jeż zajada marchewkę")
+    elif days % 2:
+        print(f"Dzień {days}: Pan Jeż zajada jabłuszko")
+    else:
+        print(f"Dzień {days}: Pan Jeż zajada gruszkę")
