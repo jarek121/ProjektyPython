b2a649b42e812b14c82d4b1224efe44fc37dcb26
Jarosław Hyżyk <jaroslawhyzyk@gmail.com>
2026-05-10 22:10:46 +0200

    zmiana6

--- www/pluszaki_app/admin.py
+++ www/pluszaki_app/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from .models import Pluszak
 
-admin.site.register(Pluszak)
-
 # Wyświetlenie listy w admin
 class PluszakAdmin(admin.ModelAdmin):
     list_display = ('imie', 'gatunek', 'zapasy')
+    
+admin.site.register(Pluszak)
+admin.site.register(Pluszak, PluszakAdmin)
