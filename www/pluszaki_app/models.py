da5792017585cd1e15890d957900a8dec0adcbd0
Jarosław Hyżyk <jaroslawhyzyk@gmail.com>
2026-05-10 21:01:09 +0200

    add .gitignore

--- www/pluszaki_app/models.py
+++ www/pluszaki_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Pluszak(models.Model):
+    imie = models.CharField(max_length=100)
+    gatunek = models.CharField(max_length=100)
+    opis = models.TextField(blank=True)
+    data_dodania = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.imie
+
