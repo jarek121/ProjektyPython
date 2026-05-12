8c6adc1055f379ed92a0ddc3a43fffbe57494bd7
Jarosław Hyżyk <jaroslawhyzyk@gmail.com>
2026-05-12 17:48:33 +0200

    add pluszaki.html testowo

--- www/pluszaki_app/views.py
+++ www/pluszaki_app/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from .models import Pluszak
+from django.http import HttpResponse
 
 def lista_pluszakow(request):
     wszystkie_pluszaki = Pluszak.objects.all() 
     return render(request, 'pluszak.html', {'Pluszaki': wszystkie_pluszaki})
+# Funkcja tymczasowa do wyświetlenia widoku 
 
+# def lista_pluszakow(request):
+#     return HttpResponse("Cześć! To działa, tutaj będą Twoje pluszaki.")
