3084e94f9309e85bde9b59a7e83f75634011aa72
Jarosław Hyżyk <jaroslawhyzyk@gmail.com>
2026-05-12 11:24:43 +0200

    dodanie widoku dla pluszaków

--- www/pluszaki_app/views.py
+++ www/pluszaki_app/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Pluszak  # importujesz swój model
 
-# Create your views here.
+def lista_zadan(request):
+    wszystkie_pluszaki = Pluszak.objects.all() 
+    return render(request, 'pluszak.html', {'Pluszaki': wszystkie_pluszaki})
+
