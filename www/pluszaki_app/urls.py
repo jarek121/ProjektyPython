52b5944c0f04ba17103412430835af3ee2454d5a
Jarosław Hyżyk <jaroslawhyzyk@gmail.com>
2026-05-12 21:28:29 +0200

    próba zmian

--- www/pluszaki_app/urls.py
+++ www/pluszaki_app/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('test/', views.lista_pluszakow, name='test_widok'),
+    path('ekipa/', views.lista_pluszakow, name='test_widok'),
 ]
